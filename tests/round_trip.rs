01c840df13f6c2e5430c32008bb8d0193d27be24
li <li@goldragon.criome.net>
2026-05-23 00:52:32 +0200

    owner-signal-persona-terminal: refresh bracket-string NOTA

--- tests/round_trip.rs
+++ tests/round_trip.rs
@@ -193,25 +193,25 @@ fn owner_terminal_canonical_examples_round_trip() {
             environment: Vec::new(),
             working_directory: None,
         }),
-        "(CreateSession operator (TerminalCommand pi []) [] None)",
+        "(CreateSession (operator (pi []) [] None))",
     );
     round_trip_nota(
         OwnerTerminalRequest::RetireSession(RetireSession { name: terminal() }),
-        "(RetireSession operator)",
+        "(RetireSession (operator))",
     );
     round_trip_nota(
         OwnerTerminalReply::SessionCreated(SessionCreated {
             name: terminal(),
             data_socket_path: data_socket_path(),
         }),
-        "(SessionCreated operator \"/run/persona/terminal/sessions/operator/data.sock\")",
+        "(SessionCreated (operator [/run/persona/terminal/sessions/operator/data.sock]))",
     );
     round_trip_nota(
         OwnerTerminalReply::SessionRetired(SessionRetired {
             name: terminal(),
             exit_status: Some(TerminalExitStatus::StatusUnavailable),
         }),
-        "(SessionRetired operator (StatusUnavailable))",
+        "(SessionRetired (operator (Some (StatusUnavailable))))",
     );
     round_trip_nota(
         OwnerTerminalReply::OwnerTerminalRequestUnimplemented(OwnerTerminalRequestUnimplemented {
@@ -219,6 +219,6 @@ fn owner_terminal_canonical_examples_round_trip() {
             operation: OwnerTerminalOperationKind::CreateSession,
             reason: OwnerTerminalUnimplementedReason::NotBuiltYet,
         }),
-        "(OwnerTerminalRequestUnimplemented operator CreateSession NotBuiltYet)",
+        "(OwnerTerminalRequestUnimplemented (operator CreateSession NotBuiltYet))",
     );
 }
